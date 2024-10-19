d933b45ef98152c27cf7deb876b5a606b696ec54
SANTIAGO-L <santigocom053@gmail.com>
2024-10-19 11:45:30 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var nombre = "santiago";
+let apellido = "victoria";
+var objeto = "moto";
+const genero = "masculino";
+var numTel = "1234567890";
+
+let frutas = ["manzana","pera","uva","sandia"];
+let verduras = ["lechuga","tomate","cebolla","papa"];
+let carnes = ["res","cerdo","pollo","pescado"];
+let numeros = [1,2,3,4,5,6,7,8,9,10];
+
+
+
+
+const persona = {
+    nombre: "santiago",
+    apellido: "victoria",
+    edad:18, 
+    genero: "masculino",
+    numTel: 1234567890,
+}
+
+
+console.log("hola",nombre);
+console.log("tu apelido es",apellido)
+console.log("tienes",objeto);
+console.log(persona);
+console.log("1"+ 1);
+console.log(frutas[2]);
